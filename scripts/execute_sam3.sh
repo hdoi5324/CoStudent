6f7432bfb0358f62ca8a30e45dbab7f97ece390d
heather <hdoi5324@uni.sydney.edu.au>
2026-05-03 10:05:05 +1000

    update for nectar

--- scripts/execute_sam3.sh
+++ scripts/execute_sam3.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-lrs=(0.01) # 0.02 for FR batch 16 0.01 for FCOS batch 16 check lr for DETR 0.0001??
+lrs=(0.005 0.001) # 0.02 for FR batch 16 0.01 for FCOS batch 16 check lr for DETR 0.0001??
 ims_per_gpu=16
-SEEDS=( 2234575 3234575) #
+SEEDS=(1234575 2234575 3234575) #
 
 species="anemone" # redcup urchin ob
 lr=0.01
-
 for SEED in "${SEEDS[@]}"
 do
-  bash scripts/train_with_dataset.sh --train-images /media/data/phd_data/loose_sam3/20260421-173150_redcup-sparse-coco/images \
-  --train-json /media/data/phd_data/loose_sam3/20260421-173150_redcup-sparse-coco/sparse_point_coco.json \
-  --test-images /media/data/phd_data/squidle_coco/squidle_redcup_test/test2023 \
-  --test-json /media/data/phd_data/squidle_coco/squidle_redcup_test/annotations/instances_test.json \
-  -- OUTPUT_DIR outputs/redcup_sam3_og_giou_${lr}_${SEED} SOLVER.OPTIMIZER.BASE_LR ${lr} SOLVER.IMS_PER_DEVICE ${ims_per_gpu} SEED ${SEED}
+  for lr in "${lrs[@]}"
+  do
+    bash scripts/train_with_dataset.sh --train-images /media/data/phd_data/loose_sam3/20260421-173150_redcup-sparse-coco/images \
+    --train-json /media/data/phd_data/loose_sam3/20260421-173150_redcup-sparse-coco/sparse_point_coco.json \
+    --test-images /media/data/phd_data/squidle_coco/squidle_redcup_test/test2023 \
+    --test-json /media/data/phd_data/squidle_coco/squidle_redcup_test/annotations/instances_test.json \
+    -- OUTPUT_DIR outputs/redcup_sam3_og_giou_${lr}_${SEED} SOLVER.OPTIMIZER.BASE_LR ${lr} SOLVER.IMS_PER_DEVICE ${ims_per_gpu} SEED ${SEED}
+  done
 done
